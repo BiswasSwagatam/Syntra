19e41912a62e319805e9419548c44cd82a8a6e6d
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2025-03-31 15:17:31 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
